2de79527574f3d5f98407683fe14b185175eb079
SeanQuinn <Seanquinn917@gmail.com>
2023-11-08 11:39:24 -0500

    commit tree a24f1df21948ac5caa34e28f3002cc8284fe7d4e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    
+    validate :clickbait_title
+
+    def clickbait_title
+        unless title&.include?("Won't Believe") || title&.include?("Secret") || title&.match?(/Top \d+/) || title&.include?("Guess")
+          errors.add(:title, "is not sufficiently clickbait-y")
+        end
+      end
+    
+
+
 end
